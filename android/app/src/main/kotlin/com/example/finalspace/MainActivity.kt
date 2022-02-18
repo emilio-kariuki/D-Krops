6a503b6eb3644f8d5ab3ef2ad6bb6d47c51af850
emilio <emilio113kariuki@gmail.com>
2022-02-18 13:03:28 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/finalspace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.finalspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:finalspace/screens/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
