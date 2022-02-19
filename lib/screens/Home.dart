dff401daef6c7cff99ab7f571dd882aa0c64087d
emilio <emilio113kariuki@gmail.com>
2022-02-19 20:50:18 +0300

    February 19, 2022 8:50 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -211,7 +211,7 @@ class _HomeState extends State<Home> {
                 gradient: LinearGradient(
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
-                  colors: [Color.fromARGB(255, 146, 252, 252), Color.fromARGB(255, 1, 170, 192)]),
+                  colors: [Color.fromARGB(255, 15, 102, 41), Color.fromARGB(255, 31, 40, 44)]),
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30))
               ),
               child: Column(
