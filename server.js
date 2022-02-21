eb64833623b416016935aed4c646a0eb8ea29531
emilio <emilio113kariuki@gmail.com>
2022-02-21 14:39:00 +0300

    February 21, 2022 2:39 PM

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const client = require('./post');
 app.post('/',(req,res) =>{
     try{
         const {type,x_coordinate,y_coordinate} = req.body;
-        const dbt = client.query("INSERT INTO emilio(first_name,second_name)VALUES($1,$2)",[first_name,second_name]);
+        const dbt = client.query("INSERT INTO space(type,x_coordinate,y_coordinate)VALUES($1,$2,$3)",[type,x_coordinate,y_coordinate]);
         res.json(dbt);
 
     }catch(err){
