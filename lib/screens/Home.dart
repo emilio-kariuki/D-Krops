6f362b44ab1d597d154ce0f1362056b50d5c0ef7
emilio <emilio113kariuki@gmail.com>
2022-02-21 10:22:15 +0300

    February 21, 2022 10:22 AM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -518,6 +518,7 @@ class _HomeState extends State<Home> {
                   onPressed: () {},
                         ),
                 ),
+                SizedBox(height: size.height * 0.02),
                 ],
                 ),
               )
