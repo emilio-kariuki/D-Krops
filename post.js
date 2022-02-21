2124e91493edf8e0220485bc815fef231ac4ac4c
emilio <emilio113kariuki@gmail.com>
2022-02-21 15:18:13 +0300

    February 21, 2022 3:18 PM

--- post.js
+++ post.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 
 const client = new Client({
     host: "localhost",
-    password: "postgress",
+    password: "postgres",
     port: 5432,
     database:"Space",
     user:"postgres"
