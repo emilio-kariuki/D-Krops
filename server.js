d2d6cec41f9c49f83bcf3186763737af35306c68
emilio <emilio113kariuki@gmail.com>
2022-02-23 16:14:51 +0300

    February 23, 2022 4:14 PM

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const client = require('./post');
 app.post('/',(req,res) =>{
     try{
         const {type,x_coordinate,y_coordinate} = req.body;
-        const dbt = client.query("INSERT INTO space(type,x_coordinate,y_coordinate,geom)VALUES($1,$2,$3,$4)",[type,x_coordinate,y_coordinate,ST_GeomFromText('POINT(-73.985744 40.748549)',4269)]);
+        const dbt = client.query("INSERT INTO space(type,x_coordinate,y_coordinate,geom)VALUES($1,$2,$3,$4)",
+        [type,x_coordinate,y_coordinate,ST_GeomFromText('POINT(-73.985744 40.748549)',4269)]);
         res.json(dbt);
 
     }catch(err){
