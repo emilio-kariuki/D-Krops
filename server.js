91e038f724cd25549777b2fafed09a477e35f87a
emilio <emilio113kariuki@gmail.com>
2022-02-26 14:20:05 +0300

    February 26, 2022 2:20 PM

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const client = require('./post');
 
 app.post('/',(req,res) =>{
     try{
-        const {x_coordinate,y_coordinate,lat,long} = req.body;
-        const dbt = client.query("INSERT INTO upload(x_coordinate,y_coordinate,geom)VALUES($1,$2,$3,$4)",
-        [x_coordinate,y_coordinate,format('ST_SetSRID(ST_MakePoint( ${lat}, $long), 4326)')]);
+        const {x_coordinate,y_coordinate,geom} = req.body;
+        const dbt = client.query("INSERT INTO upload(x_coordinate,y_coordinate,geom)VALUES($1,$2,$3)",
+        [x_coordinate,y_coordinate,format('ST_SetSRID(ST_MakePoint(36.95989172896912,-0.3975631852747608), 4326)')]);
         res.json(dbt);
     }catch(err){
         console.log(err.message)
