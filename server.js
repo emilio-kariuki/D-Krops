4f46bc1a36a9d83884cb9a26d2da60f2eec98096
emilio <emilio113kariuki@gmail.com>
2022-02-26 14:09:17 +0300

    February 26, 2022 2:09 PM

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.post('/',(req,res) =>{
     try{
         const {x_coordinate,y_coordinate,lat,long} = req.body;
         const dbt = client.query("INSERT INTO upload(x_coordinate,y_coordinate,geom)VALUES($1,$2,$3,$4)",
-        [x_coordinate,y_coordinate,format('ST_SetSRID(ST_MakePoint(${lat}, $long), 4326)')]);
+        [x_coordinate,y_coordinate,format('ST_SetSRID(ST_MakePoint($${lat}, $long), 4326)')]);
         res.json(dbt);
 
     }catch(err){
