d97c4da183682e8710fc4a0fd6948258fd0d2207
emilio <emilio113kariuki@gmail.com>
2022-02-26 12:52:04 +0300

    February 26, 2022 12:52 PM

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const client = require('./post');
 
 app.post('/',(req,res) =>{
     try{
-        const {type,x_coordinate,y_coordinate,pic,lat,long,} = req.body;
+        const {type,x_coordinate,y_coordinate,pic_path,lat,long,} = req.body;
         const dbt = client.query("INSERT INTO upload(x_coordinate,y_coordinate,picture,geom,type)VALUES($1,$2,$3,$4,$5)",
         [x_coordinate,y_coordinate,]);
         res.json(dbt);
