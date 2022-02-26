6c4919942235553c3be9796848f3a2d4b75b2ffd
emilio <emilio113kariuki@gmail.com>
2022-02-26 14:04:29 +0300

    February 26, 2022 2:04 PM

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var format = require('pg-format');
 const app = express();
 app.use(express.json({extended: false}));
 const port = 5000;
@@ -9,7 +10,7 @@ app.post('/',(req,res) =>{
     try{
         const {x_coordinate,y_coordinate,lat,long} = req.body;
         const dbt = client.query("INSERT INTO upload(x_coordinate,y_coordinate,picture,geom,type)VALUES($1,$2,$3,$4)",
-        [x_coordinate,y_coordinate,pgp.as.format('ST_SetSRID(ST_MakePoint($lat, $long), 4326)')]);
+        [x_coordinate,y_coordinate,format('ST_SetSRID(ST_MakePoint($lat, $long), 4326)')]);
         res.json(dbt);
 
     }catch(err){
