74af89a9ca30899f1fd011a3e9fc6721e211c22f
emilio <emilio113kariuki@gmail.com>
2022-02-28 19:02:33 +0300

    February 28, 2022 7:02 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_local_variable, curly_braces_in_flow_control_structures
+// ignore_for_file: unused_local_variable, curly_braces_in_flow_control_structures, avoid_print, duplicate_ignore
 
 import 'dart:convert';
 import 'dart:io';
