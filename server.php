f7f65d5da1403ff782b40ca757adc7e6fdaffa81
emilio <emilio113kariuki@gmail.com>
2022-03-02 00:13:36 +0300

    March 2, 2022 12:13 AM

--- server.php
+++ server.php
@@ -20,8 +20,57 @@
 $val = isset($_POST["x_coordinate"]) && isset($_POST["y_coordinate"])
          && isset($_POST["lat"]) && isset($_POST["long"]) && isset($_POST["type"]);
 
+         if($val){
+            //checking if there is POST data
+     
+            $x_coordinate = $_POST["x_coordinate"]; //grabing the data from headers
+            $y_coordinate = $_POST["y_coordinate"];
+            $lat = $_POST["lat"];
+            $long = $_POST["long"];
+            $type = $_POST["type"];
+     
+            //validation name if there is no error before
+            if($return["error"] == false ){
+                $return["error"] = true;
+                $return["message"] = "Enter name up to 3 characters.";
+            }
+     
+            //add more validations here
+     
+            //if there is no any error then ready for database write
+            if($return["error"] == false){
+                 $x_coordinate = mysqli_real_escape_string($link, $x_coordinate);
+                 $y_coordinate = mysqli_real_escape_string($link, $y_coordinate);
+                 $lat = mysqli_real_escape_string($link, $lat);
+                 $long = mysqli_real_escape_string($link, $long);
+                 $type = mysqli_real_escape_string($link, $type);
+                 //escape inverted comma query conflict from string
+     
+                 $sql = "INSERT INTO student_list SET
+                                     x_coordinate = '$x_coordinate',
+                                     y_coordinate = '$y_coordinate',
+                                     geom = '$class',
+                                     roll_no = '$rollno'";
+                 //student_id is with AUTO_INCREMENT, so its value will increase automatically
+     
+                 $res = mysqli_query($link, $sql);
+                 if($res){
+                     //write success
+                 }else{
+                     $return["error"] = true;
+                     $return["message"] = "Database error";
+                 }
+            }
+       }else{
+           $return["error"] = true;
+           $return["message"] = 'Send all parameters.';
+       }
+     
 
-$sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values(-122.222657, 0.2323223, ST_SetSRID(ST_MakePoint(-122.222657, 0.2323223), 4326),'maize')";
+
+
+
+
 $result = pg_query($db, $sql);
 if(!$result){
   echo pg_last_error($dbconn);
@@ -29,4 +78,5 @@ if(!$result){
   echo "Inserted successfully";
 }
 pg_close($db); 
+
 ?>
