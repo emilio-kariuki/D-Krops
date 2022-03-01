064c9fabb9d5976b4d052d2cb987cb7985490b61
emilio <emilio113kariuki@gmail.com>
2022-03-01 21:09:50 +0300

    March 1, 2022 9:09 PM

--- server.php
+++ server.php
@@ -1,3 +1,13 @@
 <?php
-    $db_connection = pg_connect("host=localhost dbname=dbname user=username password=password");
+   $host        = "host = 127.0.0.1";
+   $port        = "port = 5432";
+   $dbname      = "dbname = testdb";
+   $credentials = "user = postgres password=pass123";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 ?>
