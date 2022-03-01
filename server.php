52c68b75d7942fe2fcea4681f884390b862d7c74
emilio <emilio113kariuki@gmail.com>
2022-03-02 00:34:39 +0300

    March 2, 2022 12:34 AM

--- server.php
+++ server.php
@@ -17,8 +17,9 @@
        $lat = $_POST['lat'];
        $long = $_POST['long'];
        $type = $_POST['type'];
-       $sq = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+       $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
       //  values ('$first_name','$last_name','$city_name','$email')";
+       $result = pg_query($db, $sql);
        if($result = pg_query($query)){
          echo "Data Added Successfully.";
        }
@@ -28,13 +29,13 @@
    }
 
 
-$sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values(-122.222657, 0.2323223, ST_SetSRID(ST_MakePoint(-122.222657, 0.2323223), 4326),'maize')";
-$result = pg_query($db, $sql);
-if(!$result){
-  echo pg_last_error($dbconn);
-} else {
-  echo "Inserted successfully";
-}
+// $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values(-122.222657, 0.2323223, ST_SetSRID(ST_MakePoint(-122.222657, 0.2323223), 4326),'maize')";
+// $result = pg_query($db, $sql);
+// if(!$result){
+//   echo pg_last_error($dbconn);
+// } else {
+//   echo "Inserted successfully";
+// }
 pg_close($db); 
 
 ?>
