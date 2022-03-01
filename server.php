2f782fb88c923f3712d7d1633c0557a0c434feb1
emilio <emilio113kariuki@gmail.com>
2022-03-02 00:34:01 +0300

    March 2, 2022 12:34 AM

--- server.php
+++ server.php
@@ -10,64 +10,22 @@
    } else {
       echo "Opened database successfully\n";
    }
-   // $query = "SELECT * FROM upload"; 
-
-// $rs = pg_query($db, $query) or die("Cannot execute query: $query\n");
-
-// while ($row = pg_fetch_row($rs)) {
-//   echo "$row[0] $row[1] $row[2]\n";
-// }
-$val = isset($_POST["x_coordinate"]) && isset($_POST["y_coordinate"])
-         && isset($_POST["lat"]) && isset($_POST["long"]) && isset($_POST["type"]);
-
-         if($val){
-            //checking if there is POST data
-     
-            $x_coordinate = $_POST["x_coordinate"]; //grabing the data from headers
-            $y_coordinate = $_POST["y_coordinate"];
-            $lat = $_POST["lat"];
-            $long = $_POST["long"];
-            $type = $_POST["type"];
-     
-            //validation name if there is no error before
-            if($return["error"] == false ){
-                $return["error"] = true;
-                $return["message"] = "Enter name up to 3 characters.";
-            }
-     
-            //add more validations here
-     
-            //if there is no any error then ready for database write
-            if($return["error"] == false){
-                 $x_coordinate = mysqli_real_escape_string($link, $x_coordinate);
-                 $y_coordinate = mysqli_real_escape_string($link, $y_coordinate);
-                 $lat = mysqli_real_escape_string($link, $lat);
-                 $long = mysqli_real_escape_string($link, $long);
-                 $type = mysqli_real_escape_string($link, $type);
-                 //escape inverted comma query conflict from string
-     
-                 $sql = "INSERT INTO student_list SET
-                                     full_name = '$name',
-                                     address = '$address',
-                                     class = '$class',
-                                     roll_no = '$rollno'";
-                 //student_id is with AUTO_INCREMENT, so its value will increase automatically
-     
-                 $res = mysqli_query($link, $sql);
-                 if($res){
-                     //write success
-                 }else{
-                     $return["error"] = true;
-                     $return["message"] = "Database error";
-                 }
-            }
-       }else{
-           $return["error"] = true;
-           $return["message"] = 'Send all parameters.';
+   if(isset($_POST['save']))
+   {	 
+       $x_coordinate = $_POST['x_coordinate'];
+       $y_coordinate = $_POST['y_coordinate'];
+       $lat = $_POST['lat'];
+       $long = $_POST['long'];
+       $type = $_POST['type'];
+       $sq = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+      //  values ('$first_name','$last_name','$city_name','$email')";
+       if($result = pg_query($query)){
+         echo "Data Added Successfully.";
        }
-     
-
-
+       else{
+         echo "Error.";
+       }
+   }
 
 
 $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values(-122.222657, 0.2323223, ST_SetSRID(ST_MakePoint(-122.222657, 0.2323223), 4326),'maize')";
