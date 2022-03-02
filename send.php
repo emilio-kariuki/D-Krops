294428f46642de0bac6717840cee6283497ad8cd
emilio <emilio113kariuki@gmail.com>
2022-03-02 13:21:38 +0300

    March 2, 2022 1:21 PM

--- send.php
+++ send.php
@@ -33,8 +33,15 @@ if(!$db) {
 
         file_put_contents($upload_path,base64_decode($image));
 
-  $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
-  $result= pg_query($db,$sql_details);
+//   $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
+
+  $x_coordinate = -122.222657;
+       $y_coordinate = 0.2323223;
+       $lat = -122.222657;
+       $long = 0.2323223;
+       $type = 'maize';
+       $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+  $result= pg_query($db,$sql);
 // $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
  // $result=pg_query($q);
 
