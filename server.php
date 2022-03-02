1a1db6dc9df4b418d52a0d6b8c663dd23b2a4afe
emilio <emilio113kariuki@gmail.com>
2022-03-02 13:35:57 +0300

    March 2, 2022 1:35 PM

--- server.php
+++ server.php
@@ -12,37 +12,46 @@
    }
    
       echo "opening";
-       $x_coordinate = $_POST['x_coordinate'];
-       $y_coordinate = $_POST['y_coordinate'];
-       $lat = $_POST['lat'];
-       $long = $_POST['long'];
-       $type = $_POST['type'];
-      // $x_coordinate = -122.222657;
-      //  $y_coordinate = 0.2323223;
-      //  $lat = -122.222657;
-      //  $long = 0.2323223;
-      //  $type = 'beans';
-       $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
-      //  values ('$first_name','$last_name','$city_name','$email')";
-       $result = pg_query($db, $sql);
-       if($result){
-         echo "Data Added Successfully.";
-       }
-       else{
-         echo "Error.";
-       }
+      $db = pg_connect( "$host $port $dbname $credentials"  );
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+      
+          // require 'init.php';
+          
+          $current_date=date("Y/m/d");
+      
+          $image=$_POST['image'];
+          $x_coordinate=$_POST['x_coordinate'];
+          $y_coordinate=$_POST['y_coordinate'];
+          $latitude=$_POST['latitude'];
+          $longitude=$_POST['longitude'];
+          $type=$_POST['type'];
+          
+          $image_name = uniqid();
+          
+         
+          $upload_path="images/$image_name.jpg";
+      
+         if($upload_path){
+      
+              file_put_contents($upload_path,base64_decode($image));
+      
+      //   $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
+      
        
-   
-   // header('Content-Type: application/json');
-
-
-// $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values(-122.222657, 0.2323223, ST_SetSRID(ST_MakePoint(-122.222657, 0.2323223), 4326),'maize')";
-// $result = pg_query($db, $sql);
-// if(!$result){
-//   echo pg_last_error($dbconn);
-// } else {
-//   echo "Inserted successfully";
-// }
-pg_close($db); 
+             $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+        $result= pg_query($db,$sql);
+      // $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
+       // $result=pg_query($q);
+      
+              echo json_encode(array('response'=>'Information Sent Successfully'));
+          }else{
+      
+              echo json_encode(array('response'=>'Information NOT Sent'));
+          }
+// pg_close($db); 
 
 ?>
