33b632763bfbe1f6933af56cc173446c1c6e3007
emilio <emilio113kariuki@gmail.com>
2022-03-02 13:28:26 +0300

    March 2, 2022 1:28 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -702,7 +702,7 @@ class _HomeState extends State<Home> {
   _makeGetRequest() async {
     final url = Uri.parse(_localhost());
     Response response =
-        await post(Uri.parse('http://10.0.2.2/server.php'),
+        await post(Uri.parse('http://172.16.121.173/server.php'),
             headers: {"content-type": "application/json"},
             body: jsonEncode({
               "type": selectedType,
