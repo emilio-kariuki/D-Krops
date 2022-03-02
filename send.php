572ef48f778c3ab99be1ce6760a3487ff36722c5
emilio <emilio113kariuki@gmail.com>
2022-03-02 14:06:26 +0300

    March 2, 2022 2:06 PM

--- send.php
+++ send.php
@@ -15,36 +15,36 @@ if(!$db) {
 
     // require 'init.php';
     
-    $current_date=date("Y/m/d");
+//     $current_date=date("Y/m/d");
 
-    $image=$_POST['image'];
-    $x_coordinate=$_POST['x_coordinate'];
-    $y_coordinate=$_POST['y_coordinate'];
-    $latitude=$_POST['latitude'];
-    $longitude=$_POST['longitude'];
-    $type=$_POST['type'];
+//     $image=$_POST['image'];
+//     $x_coordinate=$_POST['x_coordinate'];
+//     $y_coordinate=$_POST['y_coordinate'];
+//     $latitude=$_POST['latitude'];
+//     $longitude=$_POST['longitude'];
+//     $type=$_POST['type'];
     
-    $image_name = uniqid();
+//     $image_name = uniqid();
     
    
-    $upload_path="images/$image_name.jpg";
+//     $upload_path="images/$image_name.jpg";
 
-   if($upload_path){
+//    if($upload_path){
 
-        file_put_contents($upload_path,base64_decode($image));
+//         file_put_contents($upload_path,base64_decode($image));
 
-//   $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
+// //   $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
 
  
-       $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+       $sql = "SELECT * FROM upload";
   $result= pg_query($db,$sql);
 // $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
  // $result=pg_query($q);
 
-        echo json_encode(array('response'=>'Information Sent Successfully'));
-    }else{
+    //     echo json_encode(array('response'=>'Information Sent Successfully'));
+    // }else{
 
-        echo json_encode(array('response'=>'Information NOT Sent'));
-    }
+    //     echo json_encode(array('response'=>'Information NOT Sent'));
+    // }
 
 ?>
