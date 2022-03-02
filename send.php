0e198be8d2cd77f4e01cae0c165cf224e6b141af
emilio <emilio113kariuki@gmail.com>
2022-03-02 11:06:28 +0300

    March 2, 2022 11:06 AM

---
+++ send.php
@@ -0,0 +1,47 @@
+
+<?php
+
+$host        = "host = localhost";
+$port        = "port = 5432";
+$dbname      = "dbname = Agent";
+$credentials = "user = postgres password=postgres";
+
+$db = pg_connect( "$host $port $dbname $credentials"  );
+if(!$db) {
+   echo "Error : Unable to open database\n";
+} else {
+   echo "Opened database successfully\n";
+}
+
+    require 'init.php';
+    
+    $current_date=date("Y/m/d");
+
+    $image=$_POST['image'];
+    $description=$_POST['description'];
+    $incident_type=$_POST['incident_type'];
+    $latitude=$_POST['latitude'];
+    $longitude=$_POST['longitude'];
+    $my_id=rand(100,1000);
+    
+    $image_name = uniqid();
+    
+   
+    $upload_path="images/$image_name.jpg";
+
+   if($upload_path){
+
+        file_put_contents($upload_path,base64_decode($image));
+
+  $sql_details="INSERT INTO sent_information (id,user_id,description,incident_type,latitude,longitude,image_name,date_sent,status) VALUES ('$my_id',0,'$description','$incident_type','$latitude','$longitude','$image_name','$current_date','Pending')";
+  $result= pg_query($db,$sql_details);
+// $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
+ // $result=pg_query($q);
+
+        echo json_encode(array('response'=>'Information Sent Successfully'));
+    }else{
+
+        echo json_encode(array('response'=>'Information NOT Sent'));
+    }
+
+?>
