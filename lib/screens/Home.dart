e6a87fdd4f5ee85c4d5c49e76dcfbc4fbdfd3fd3
emilio <emilio113kariuki@gmail.com>
2022-03-03 22:15:37 +0300

    March 3, 2022 10:15 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -716,7 +716,7 @@ class _HomeState extends State<Home> {
 
   String _localhost() {
     if (Platform.isAndroid)
-      return 'http://10.0.2.2:5000/';
+      return 'http://10.0.2.2/';
     else // for iOS simulator
       return 'http://localhost:5000/';
   }
