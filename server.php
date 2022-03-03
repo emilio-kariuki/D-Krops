f9cfdfd4f673831803f8778f6ac997474f60f3b6
emilio <emilio113kariuki@gmail.com>
2022-03-03 17:22:20 +0300

    March 3, 2022 5:22 PM

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-   $host        = "host = https://iggresapps.dkut.ac.ke/crop_mapping.php";
+   $host        = "host = localhost";
    $port        = "port = 5432";
    $dbname      = "dbname = Agent";
    $credentials = "user = postgres password=postgres";
@@ -14,7 +14,7 @@
       echo "opening";
                // require 'init.php';
           
-          $current_date=date("Y/m/d");
+         //  $current_date=date("Y/m/d");
       
           $image=$_POST['image'];
           $x_coordinate=$_POST['x_coordinate'];
@@ -30,12 +30,12 @@
       
          if($upload_path){
       
-              file_put_contents($upload_path,base64_decode($image));
+            //   file_put_contents($upload_path,base64_decode($image));
       
       //   $sql_details="INSERT INTO upload (x_coordinate,y_coordinate,geom,type) VALUES ($x_coordinate,$y_coordinate,ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),$type)";
       
        
-             $sql = "insert into upload (x_coordinate, y_coordinate,geom,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($long, $lat), 4326),'$type')";
+             $sql = "insert into upload (x_coordinate, y_coordinate,geom,picture,type)  values($x_coordinate, $y_coordinate, ST_SetSRID(ST_MakePoint($longitude, $latitude), 4326),'$upload_path','$type')";
         $result= pg_query($db,$sql);
       // $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
        // $result=pg_query($q);
