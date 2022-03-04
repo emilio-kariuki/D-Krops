307ad76993fa0364d14d7ea294a197ffab51765e
emilio <emilio113kariuki@gmail.com>
2022-03-04 10:57:49 +0300

    March 4, 2022 10:57 AM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -702,7 +702,7 @@ class _HomeState extends State<Home> {
   _makeGetRequest() async {
     final url = Uri.parse('https://iggresapps.dkut.ac.ke/crop_mapping.php');
     Response response = await post(url,
-        headers: {"content-type": "application/json"},
+        // headers: {"content-type": "application/json"},
         body: jsonEncode({
           "x_coordinate": latitude,
           "y_coordinate": longitude,
