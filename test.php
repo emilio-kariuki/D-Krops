5f4414943c2762c472986c14790586286553b34d
emilio <emilio113kariuki@gmail.com>
2022-03-06 15:14:59 +0300

    March 6, 2022 3:14 PM

--- test.php
+++ test.php
@@ -3,25 +3,43 @@
     $host        = "host = iggresserver.dkut.ac.ke ";
     $port        = "port = 5432";
     $dbname      = "dbname = crop_mapping";
-    $user = "user = postgres password=.Xy!@D3K_3ev3R.2!jL!";
+    $user = "user = postgres";
+    $password= "password=.Xy!@D3K_3ev3R.2!jL!";
     
-
-    $db = pg_connect( "$host $port $dbname $user "  );
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
+  
+    $db = new PDO($dsn);
     if(!$db) {
     echo "Error : Unable to open database\n";
     } else {
     echo "Opened database successfully\n";
     }
 
-    echo "opening";
+    $image=$_POST['image'];
+    $x_coordinate=$_POST['x_coordinate'];
+    $y_coordinate=$_POST['y_coordinate'];
+    $latitude=$_POST['latitude'];
+    $longitude=$_POST['longitude'];
+    $type=$_POST['type'];
+    
+    $image_name = uniqid();
+    
+   
+    $upload_path="images/$image_name.jpg";
 
-    
-    $name = "done";
-    
-    
+   if($upload_path){
 
-  $sql="insert into table_test(name) VALUES ('$name')";
-  $result= pg_query($db,$sql);
+        file_put_contents($upload_path,base64_decode($image));
+
+  $sql_details="INSERT INTO crop_mapping (x_coordinate,y_coordinate,geom,picture,type) VALUES ('$x_coordinate',0,'$y_coordinate',ST_SetSRID(ST_MakePoint('$longitude', '$latitude'), 4326),'$type')";
+  $result= pg_query($db,$sql_details);
 // $q=($db,"INSERT INTO sent_information(id, user_id, description, incident_type, latitude, longitude, image_name, date_sent, status)VALUES ('0', '0', 'df', 'jh', 'hg', 'jf', 'gfd', 'fds', 'erd')"); 
  // $result=pg_query($q);
+
+        echo json_encode(array('response'=>'Information Sent Successfully'));
+    }else{
+
+        echo json_encode(array('response'=>'Information NOT Sent'));
+    }
+
 ?>
