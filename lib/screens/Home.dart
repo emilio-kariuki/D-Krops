82e035e116b1727af4235dee75b92a06594a235c
emilio <emilio113kariuki@gmail.com>
2022-03-07 17:58:29 +0300

    March 7, 2022 5:58 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -715,6 +715,7 @@ class _HomeState extends State<Home> {
                       // Future.delayed(const Duration(milliseconds: 1670),
                       //     () => Navigator.of(context).pop());
                       setState(() {});
+                      selectedType = null;
                     },
                   ),
                 ),
