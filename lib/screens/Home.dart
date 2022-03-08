9d650d36dbe6d10855e62b1221bca3e0ffeb2223
emilio <emilio113kariuki@gmail.com>
2022-03-08 19:33:09 +0300

    March 8, 2022 7:33 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -619,7 +619,7 @@ class _HomeState extends State<Home> {
                           child: Row(
                             children: [
                               LottieContain(
-                                  lottieUrl: "assets/lottie/select.json"),
+                                  lottieUrl: "assets/lottie/row.json"),
                               SizedBox(width: size.width * 0.06),
                               Flexible(
                                 flex:1,
