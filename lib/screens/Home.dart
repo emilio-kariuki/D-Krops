9b8141acaf3493eaf8a3f126d4b5ade4f5d4c42c
emilio <emilio113kariuki@gmail.com>
2022-03-08 19:15:37 +0300

    March 8, 2022 7:15 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -786,8 +786,43 @@ class _HomeState extends State<Home> {
                             ],
                           ),
                         ),
-                        
-                        
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, bottom: 8, top: 5),
+                          child: Row(
+                            children: [
+                              LottieContain(
+                                  lottieUrl: "assets/lottie/select.json"),
+                              SizedBox(width: size.width * 0.06),
+                              Flexible(
+                                flex:1,
+                                child: TextFormField(
+                                  keyboardType: TextInputType.multiline,
+                                  // style: TextStyle(height:10),
+                                  
+                                  decoration: InputDecoration(
+                                      isDense: true, 
+                                      contentPadding: EdgeInsets.fromLTRB(15, 20, 15, 15),
+                                      // contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                      ),
+                                      // icon: Icon(Icons.person,size:30,color: Colors.black),
+                                      filled: true,
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[800]),
+                                      focusColor: Colors.red,
+                                      hintText: "Height",
+                                      // suffixText: "10",
+                                      // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                                      fillColor: Colors.grey[200]),
+                                  controller: height,
+                                ),
+                              ),
+                              SizedBox(width:15),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
