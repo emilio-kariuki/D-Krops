70656c2fb42a52ce7fca8bab785e2ba7e41b0f9c
emilio <emilio113kariuki@gmail.com>
2022-03-12 13:09:00 +0300

    March 12, 2022 1:09 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -895,7 +895,7 @@ class _HomeState extends State<Home> {
                                     margin: const EdgeInsets.only(
                                         left: 20.0, right: 20.0),
                                     child: Divider(
-                                      color: Color.fromARGB(255, 0, 0, 0),
+                                      color: Color.fromARGB(255, 247, 247, 247),
                                       height: 5,
                                       thickness: 0.4,
                                     )),
