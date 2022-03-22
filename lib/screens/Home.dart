ff6033fa5fa84912d7b811a8653218185d74325f
emilio <emilio113kariuki@gmail.com>
2022-03-22 12:37:18 +0300

    March 22, 2022 12:37 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1181,7 +1181,7 @@ class _HomeState extends State<Home> {
                               if (selectedType == null && image == null) {
                                 Fluttertoast.showToast(
                                     backgroundColor: Colors.red,
-                                    msg: "Valid Email",
+                                    msg: "Ensure all the fields are filled",
                                     toastLength: Toast.LENGTH_SHORT,
                                     gravity: ToastGravity.BOTTOM,
                                     timeInSecForIosWeb: 1,
