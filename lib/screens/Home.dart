f725d130fe31a4f4cc436af13628ca930f9e6af3
emilio <emilio113kariuki@gmail.com>
2022-03-23 13:52:50 +0300

    March 23, 2022 1:52 PM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1234,7 +1234,7 @@ class _HomeState extends State<Home> {
                                   today == null) {
                                 Fluttertoast.showToast(
                                     backgroundColor: Colors.red,
-                                    msg: "Ensure all the fields are filled",
+                                    msg: "Fill Empty Fields",
                                     toastLength: Toast.LENGTH_SHORT,
                                     gravity: ToastGravity.BOTTOM,
                                     timeInSecForIosWeb: 1,
