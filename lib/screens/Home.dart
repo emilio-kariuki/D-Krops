98b6f27f83a63e08fc0416d0c3e5fcef3e2839c3
emilio <emilio113kariuki@gmail.com>
2022-03-30 10:17:48 +0300

    March 30, 2022 10:17 AM

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -719,6 +719,7 @@ class _HomeState extends State<Home> {
                                       } else if (selectedType == "lettuce") {
                                         unique = 41;
                                       }
+                                      print(unique);
                                     });
                                   },
                                   icon: const Icon(
